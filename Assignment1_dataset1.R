b81ea9cb0234de1c9acb67b12d292554452ddcb2
FedericoColombo <fcolomboercole@gmail.com>
2014-09-25 18:54:56 +0200

    dataset1

--- Assignment1_dataset1.R
+++ Assignment1_dataset1.R
@@ -9,15 +9,37 @@
 #Database 2
 ##############################################
 
-
+# Dataset "C02" uploaded
 data(CO2)
+
+# We took a look at the data description for the dataset CO2
 ?CO2
+
+# Variables' names and values
 names(CO2)
 head(CO2)
+
+# Explaration of variables
+summary(CO2$Plant)
+summary(CO2$Treatment)
+summary(CO2$conc)
+summary(CO2$uptake)
+
+# We focused on variable "uptake"
 hist(CO2$uptake)
 hist(CO2$uptake,
      main = 'carbon dioxide uptake rates',
      xlab = 'carbon dioxide uptake rates (umol/m 2sec)'
+     
+# We looked at the joint distribution between variables conc and uptake
 
+plot(CO2$conc, CO2$uptake,
+     main = 'Joint distribution variables',
+     xlab = 'Carbon dioxide concentration',
+     ylab = 'CO2 uptakes')
+
+# We created a new variable called conc_uptake, which is simply the ratio between variable conc and variable uptake
 CO2$conc_uptake <- CO2$conc/CO2$uptake
 summary(CO2$conc_uptake)
+median(CO2$conc_uptake)
+
