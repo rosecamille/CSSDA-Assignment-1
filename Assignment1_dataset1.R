3ece6c849861af1f3aaa8e27f3ae67abf7dc85b5
FedericoColombo <fcolomboercole@gmail.com>
2014-09-26 09:18:11 +0200

    Minor changes

--- Assignment1_dataset1.R
+++ Assignment1_dataset1.R
@@ -12,34 +12,43 @@
 # Dataset "C02" uploaded
 data(CO2)
 
-# We took a look at the data description for the dataset CO2
+# We took a look at the data description for 
+# the dataset CO2: variable Plant is an gives an identifier 
+#  value for each single plant; variable Type gives the 
+# origin of the plant, Treatmnet is a dummy variable (chilled, 
+# nonchilled), variable conc indicates the levels of carbon
+# dioxide concentrations and variable uptake indicates the 
+# levels of carbon dioxide uptake rates.
 ?CO2
 
 # Variables' names and values
 names(CO2)
 head(CO2)
 
-# Explaration of variables
+# Exploration of variables: we used the summary function to 
+# obtain an overview of the values/distributions of our variables.
 summary(CO2$Plant)
 summary(CO2$Treatment)
 summary(CO2$conc)
 summary(CO2$uptake)
 
 # We focused on variable "uptake"
-hist(CO2$uptake)
 hist(CO2$uptake,
      main = 'carbon dioxide uptake rates',
-     xlab = 'carbon dioxide uptake rates (umol/m 2sec)'
+     xlab = 'uptake rates in umol/m 2 sec')
      
-# We looked at the joint distribution between variables conc and uptake
+# We then looked at the joint distribution between variables 
+# conc and uptake
 
 plot(CO2$conc, CO2$uptake,
      main = 'Joint distribution variables',
      xlab = 'Carbon dioxide concentration',
      ylab = 'CO2 uptakes')
 
-# We created a new variable called conc_uptake, which is simply the ratio between variable conc and variable uptake
-CO2$conc_uptake <- CO2$conc/CO2$uptake
-summary(CO2$conc_uptake)
-median(CO2$conc_uptake)
+# We created a new variable called conc_over_uptake, which is simply 
+# the ratio between variable conc and variable uptake
+CO2$conc_over_uptake <- CO2$conc/CO2$uptake
+summary(CO2$conc_over_uptake)
+median(CO2$conc_over_uptake)
+plot(CO2$conc_over_uptake)
 
